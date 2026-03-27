6228a5e238a4d3f1e4d7eb3d91e0a0ad93acd4cc
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-27 17:20:15 +0100

    feat: add form

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,8 @@ export default {
     extend: {
       gridTemplateColumns: {
         app: 'minmax(18rem, 20rem) 1fr',
-        profile: 'min-content 1fr min-content'
+        profile: 'min-content 1fr min-content',
+        form: 'minmax(7.5rem, 17.5rem) minmax(25rem, 1fr) minmax(0, 15rem)',
       },
       maxWidth: {
         app: '700px'
