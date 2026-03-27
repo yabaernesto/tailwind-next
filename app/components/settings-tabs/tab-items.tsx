c3ff537bca3cfa7aeec59eb11ea4b703c8074027
Yaba Ernesto <yabaernesto@gmail.com>
2026-03-27 16:45:04 +0100

    feat: add menu-bar and tabs

---
+++ app/components/settings-tabs/tab-items.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+import * as Tabs from "@radix-ui/react-tabs";
+
+export interface TabItemProps {
+  value: string;
+  title: string;
+  isSelected?: boolean;
+}
+
+export function TabItem({ value, title, isSelected = false }: TabItemProps) {
+  return (
+    <Tabs.Trigger
+      value={value}
+      className="relative px-1 pb-4 text-sm font-medium text-zinc-500 hover:text-violet-700 data-[state=active]:text-violet-700"
+    >
+      <span>{title}</span>
+
+      {isSelected && (
+        <motion.div
+          layoutId="activeTab"
+          className="absolute right-0 -bottom-px left-0 h-0.5 bg-violet-700"
+        />
+      )}
+    </Tabs.Trigger>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
+import { SettingsTabs } from "./components/settings-tabs";
+
 export default function Home() {
   return (
-    <div className="flex h-screen flex-col items-center bg-slate-50 p-8 text-center text-slate-900 dark:bg-slate-900 dark:text-slate-50">
-      <div className="max-w-2xl">
-        <h1 className="flex items-center gap-3 text-5xl font-bold before:flex before:h-8 before:w-0.5 before:bg-sky-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </h1>
-        <p className="mt-4 dark:text-slate-400">
-          Commodi eum magni quos, eius rerum reprehenderit accusantium
-          voluptates saepe! Voluptas non quaerat, aliquam modi harum neque iste
-          repellat nihil quia facere.
-        </p>
-        <button className="mt-4 rounded-md bg-sky-500 px-4 py-2 font-medium text-white disabled:cursor-not-allowed disabled:opacity-60 disabled:hover:bg-sky-600">
-          Sign in
-        </button>
-      </div>
-    </div>
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+
+      <SettingsTabs />
+    </>
   );
 }
